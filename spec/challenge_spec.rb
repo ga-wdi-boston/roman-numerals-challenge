b383adb7b2fe25434e6de979d7ee58dcfaa1f83e
Matt Brendzel <matt.brendzel@generalassemb.ly>
2016-05-05 14:22:17 -0400

    Clean up Ruby specs using .zip
    Got the idea from my Python specs.

--- spec/challenge_spec.rb
+++ spec/challenge_spec.rb
@@ -4,27 +4,25 @@ RSpec.describe "Challenge: " do
 
   describe 'arabic_to_roman' do
     it 'produces simple roman numerals' do
-      [ [3,'III'],
-        [20,'XX'],
-        [100,'C']].each do | arabic, roman |
+      [3,20,100]
+      .zip(['III','XX','C'])
+      .each do | arabic, roman |
         expect(arabic_to_roman(arabic)).to eql(roman);
       end
     end
 
     it 'produces non-subtractive roman numerals' do
-      [ [15, 'XV'],
-        [6, 'VI'],
-        [123, 'CXXIII'],
-        [71, 'LXXI']].each do | arabic, roman |
+      [15, 6, 123, 71]
+      .zip(['XV','VI','CXXIII','LXXI'])
+      .each do | arabic, roman |
         expect(arabic_to_roman(arabic)).to eql(roman);
       end
     end
 
     it 'produces subtractive roman numerals' do
-      [ [4, 'IV'],
-        [9, 'IX'],
-        [99, 'XCIX'],
-        [405, 'CDV']].each do | arabic, roman |
+      [4,9,99,405]
+      .zip(['IV','IX','XCIX','CDV'])
+      .each do | arabic, roman |
         expect(arabic_to_roman(arabic)).to eql(roman);
       end
     end
@@ -32,27 +30,25 @@ RSpec.describe "Challenge: " do
 
   xdescribe 'roman_to_arabic' do
     it 'converts simple roman numerals into arabic numbers' do
-      [ [3,'III'],
-        [20,'XX'],
-        [100,'C']].each do | arabic, roman |
+      [3,20,100]
+      .zip(['III','XX','C'])
+      .each do | arabic, roman |
         expect(roman_to_arabic(roman)).to eql(arabic);
       end
     end
 
     it 'converts non-subtractive roman numerals into arabic numbers' do
-      [ [15, 'XV'],
-        [6, 'VI'],
-        [123, 'CXXIII'],
-        [71, 'LXXI']].each do | arabic, roman |
+      [15, 6, 123, 71]
+      .zip(['XV','VI','CXXIII','LXXI'])
+      .each do | arabic, roman |
         expect(roman_to_arabic(roman)).to eql(arabic);
       end
     end
 
     it 'converts subtractive roman numerals into arabic numbers' do
-      [ [4, 'IV'],
-        [9, 'IX'],
-        [99, 'XCIX'],
-        [405, 'CDV']].each do | arabic, roman |
+      [4,9,99,405]
+      .zip(['IV','IX','XCIX','CDV'])
+      .each do | arabic, roman |
         expect(roman_to_arabic(roman)).to eql(arabic);
       end
     end
