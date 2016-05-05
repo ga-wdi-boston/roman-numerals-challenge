2abaf1fb598a2aabe6027cca2f7646d3cb2ec1d6
Matt Brendzel <matt.brendzel@generalassemb.ly>
2016-05-05 02:55:30 -0400

    Add Rspec spec file & Ruby starter code
    `challenge.rb` has the code for two methods, arabic_to_roman and roman_to_arabic.
    Tests exist for both of these in `challenge_spec.rb`, but tests for the latter
     are hidden (`xdescribe`), to be used for the bonus.

---
+++ lib/challenge.rb
@@ -0,0 +1,8 @@
+
+def arabic_to_roman(num)
+  # Your Code Here
+end
+
+def roman_to_arabic(numeral_string)
+  # Your Code Here
+end
---
+++ spec/challenge_spec.rb
@@ -0,0 +1,61 @@
+require_relative '../lib/challenge.rb'
+
+RSpec.describe "Challenge" do
+
+  describe 'Arabic Numbers to Roman Numerals' do
+    it 'produces simple roman numerals' do
+      [ [3,'III'],
+        [20,'XX'],
+        [100,'C']].each do | arabic, roman |
+        expect(arabic_to_roman(arabic)).to eql(roman);
+      end
+    end
+
+    it 'produces non-subtractive roman numerals' do
+      [ [15, 'XV'],
+        [6, 'VI'],
+        [123, 'CXXIII'],
+        [71, 'LXXI']].each do | arabic, roman |
+        expect(arabic_to_roman(arabic)).to eql(roman);
+      end
+    end
+
+    it 'produces subtractive roman numerals' do
+      [ [4, 'IV'],
+        [9, 'IX'],
+        [99, 'XCIX'],
+        [405, 'CDV']].each do | arabic, roman |
+        expect(arabic_to_roman(arabic)).to eql(roman);
+      end
+    end
+  end
+
+  xdescribe 'Roman Numerals to Arabic Numbers' do
+    it 'converts simple roman numerals into arabic numbers' do
+      [ [3,'III'],
+        [20,'XX'],
+        [100,'C']].each do | arabic, roman |
+        expect(roman_to_arabic(roman)).to eql(arabic);
+      end
+    end
+
+    it 'converts non-subtractive roman numerals into arabic numbers' do
+      [ [15, 'XV'],
+        [6, 'VI'],
+        [123, 'CXXIII'],
+        [71, 'LXXI']].each do | arabic, roman |
+        expect(roman_to_arabic(roman)).to eql(arabic);
+      end
+    end
+
+    it 'converts subtractive roman numerals into arabic numbers' do
+      [ [4, 'IV'],
+        [9, 'IX'],
+        [99, 'XCIX'],
+        [405, 'CDV']].each do | arabic, roman |
+        expect(roman_to_arabic(roman)).to eql(arabic);
+      end
+    end
+  end
+
+end
