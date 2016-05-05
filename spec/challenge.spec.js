7a37cc4f8deba052adc765016a7e2b670f664157
Matt Brendzel <matt.brendzel@generalassemb.ly>
2016-05-05 01:35:52 -0400

    Add Jasmine spec file & JS starter code
    `challenge.js` exports two functions, arabicToRoman and romanToArabic.
    Tests exist for both of these in `challenge.spec.js`, but tests for the latter
     are hidden (`xdescribe`), to be used for the bonus.

---
+++ spec/challenge.spec.js
@@ -0,0 +1,80 @@
+'use strict';
+
+const romanNumerals = require('../lib/challenge.js');
+
+describe('challenge', () => {
+
+  describe('Arabic Numbers to Roman Numerals', () => {
+    const arabicToRoman = romanNumerals.arabicToRoman;
+    console.log(romanNumerals);
+
+    it(`produces simple roman numerals`, () => {
+      [ [3,'III'],
+        [20,'XX'],
+        [100,'C']].forEach((pair)=>{
+        let arabic = pair[0];
+        let roman = pair[1];
+        expect(arabicToRoman(arabic)).toEqual(roman);
+      });
+    });
+
+    it(`produces non-subtractive roman numerals`, () => {
+      [ [15, 'XV'],
+        [6, 'VI'],
+        [123, 'CXXIII'],
+        [71, 'LXXI']].forEach((pair)=>{
+        let arabic = pair[0];
+        let roman = pair[1];
+        expect(arabicToRoman(arabic)).toEqual(roman);
+      });
+    });
+
+    it(`produces subtractive roman numerals`, () => {
+      [ [4, 'IV'],
+        [9, 'IX'],
+        [99, 'XCIX'],
+        [405, 'CDV']].forEach((pair)=>{
+        let arabic = pair[0];
+        let roman = pair[1];
+        expect(arabicToRoman(arabic)).toEqual(roman);
+      });
+    });
+  });
+
+  xdescribe('Roman Numerals to Arabic Numbers', () => {
+    const romanToArabic = romanNumerals.romanToArabic;
+
+    it(`handles simple roman numerals`, () => {
+      [ [3,'III'],
+        [20,'XX'],
+        [100,'C']].forEach((pair)=>{
+        let arabic = pair[0];
+        let roman = pair[1];
+        expect(romanToArabic(roman)).toEqual(arabic);
+      });
+    });
+
+    it(`handles non-subtractive roman numerals`, () => {
+      [ [15, 'XV'],
+        [6, 'VI'],
+        [123, 'CXXIII'],
+        [71, 'LXXI']].forEach((pair)=>{
+        let arabic = pair[0];
+        let roman = pair[1];
+        expect(romanToArabic(roman)).toEqual(arabic);
+      });
+    });
+
+    it(`handles subtractive roman numerals`, () => {
+      [ [4, 'IV'],
+        [9, 'IX'],
+        [99, 'XCIX'],
+        [405, 'CDV']].forEach((pair)=>{
+        let arabic = pair[0];
+        let roman = pair[1];
+        expect(romanToArabic(roman)).toEqual(arabic);
+      });
+    });
+  });
+
+});
