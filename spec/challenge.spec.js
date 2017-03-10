f95c4a024017be957fa195ac81e423584c34cf00
Ben Adamski <bengitscode@gmail.com>
2017-03-10 16:38:51 -0500

    Add necessary chai and expect imports to tests
    -  Was this removed in update?

--- spec/challenge.spec.js
+++ spec/challenge.spec.js
@@ -1,5 +1,15 @@
 'use strict';
 
+// Allow chai syntax like `expect(foo).to.be.ok;`
+// jshint -W030
+
+const chai = require('chai');
+const chaiAsPromised = require('chai-as-promised');
+
+chai.use(chaiAsPromised);
+
+const expect = chai.expect;
+
 const romanNumerals = require('../lib/challenge.js');
 
 describe('Challenge: ', () => {
